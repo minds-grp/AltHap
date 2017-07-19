a1ddeb7669f67cebceaa0e3c55cf694c6204e3b9
Abolfazl Hashemi <abolfazlhashemi70@yahoo.com>
2017-07-18 22:02:33 -0500

    Update AltHap.m

--- AltHap.m
+++ AltHap.m
@@ -116,4 +116,53 @@ if ploidy > 2 && alleles_num > 2
             ,R(ii,:)),2));
     end
     Vt = Vt'-1;
+%% Formulation for Polyploid - Biallelic
+elseif ploidy > 2 && alleles_num == 2
+    % Preprocessing
+    BS = eye(ploidy);
+    Index = double(R ~= 0);
+    R = (R*2-3).*Index;
+    [read_num,hap_len] = size(R);
+    % Initialization
+    [~, ss, Vt] = svds(R,ploidy);
+    Vt = Vt*sqrt(ss);
+    Vt = sign(Vt');
+    % Optimization
+    err = inf; err_inV = inf; err_hap = inf;
+    err_hist = zeros(1,maxit);
+    Vt_last = 100*ones(ploidy,hap_len);
+    iter=0; tic
+    while iter < maxit && err > thr && err_inV > thr && err_hap > thr
+        iter = iter + 1;
+        Uh = zeros(read_num,ploidy);
+        for ii = 1:ploidy
+            Uh(:,ii) = sum((bsxfun(@minus,R,Vt(ii,:)).*Index).^2,2);
+        end
+        [~,Uh]=min(Uh,[],2);
+        Uh = BS(Uh, :);
+        G_Vt = -Uh'*((R-Uh*Vt).*Index);
+        step_size = max(0.09,(0.9*norm(G_Vt,'fro')...
+            /norm((Uh*G_Vt).*Index,'fro'))^2);
+        Vt = Vt-step_size*G_Vt;
+        Vt = max(min(Vt,1),-1);
+        % Termination criteria
+        err = norm((R-Uh*Vt).*Index,'fro'); err_hist(iter) = err;
+        if iter > 1
+            err_inV = abs(err_hist(iter) - err_hist(iter-1));
+        end
+        err_hap = norm(Vt-Vt_last,'fro')/sqrt(hap_len/ploidy);
+        Vt_last = Vt;
+    end
+    cpu_time = toc;
+    Iter_count = iter;
+    err_hist = err_hist(1:iter);    
+    % Final projection and MEC Calculation
+    MEC = 0; Vt = sign(Vt);
+    for ii = 1:read_num
+        MEC = MEC+min(sum(bsxfun(@ne,bsxfun(@times,Vt,double(R(ii,:)~= 0))...
+            ,R(ii,:)),2));
+    end
+    Vt = Vt'; Vt = (Vt+1)/2;
+%% Formulation for Diploid
+    
 
